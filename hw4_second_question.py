110d0bd4b1d4da32db76eb71397dc5322dcec050
tobinho <114157156+TobiPfeiffersGitHub@users.noreply.github.com>
2022-11-02 13:31:01 +0100

    tobinhos branch
    this is tobinhos branch

---
+++ hw4_second_question.py
@@ -0,0 +1,34 @@
+#
+# 4)
+# Create a function called "has_experience_as"
+# that has two parameters:
+# 1. A list of CV's.
+# 2. A string (job_title)
+#
+# The function should return a list of strings
+# representing the usernames of every user that
+# has worked as job_title.
+
+
+
+#
+# 5)
+# Create a function called "job_counts"
+# that has one parameter: list of CV's
+# and returns a dictionary where the
+# keys are the job titles and the values
+# are the number of users that have done
+# that job.
+
+
+
+#
+# 6)
+# Create a function, called "most_popular_job"
+# that has one parameter: a list of CV's, and
+# returns a tuple (str, int) that represents
+# the title of the most popular job and the number
+# of times it was held by people on the site.
+#
+# HINT: You should probably use your "job_counts"
+# function!
