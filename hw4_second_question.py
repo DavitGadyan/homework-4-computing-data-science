76ed46636d5f593f92ed3a70e8e03d7c34ad4389
tobinho <114157156+TobiPfeiffersGitHub@users.noreply.github.com>
2022-11-09 17:33:57 +0100

    hw4 4 to 6
    hw4 4 to 6

--- hw4_second_question.py
+++ hw4_second_question.py
@@ -1,3 +1,18 @@
+# Now, imagine you are given data from a website that
+# has people's CVs. The data comes
+# as a list of dictionaries and each
+# dictionary looks like this:
+#
+# { 'user': 'george', 'jobs': ['bar', 'baz', 'qux']}
+# e.g. [{'user': 'john', 'jobs': ['analyst', 'engineer']},
+#       {'user': 'jane', 'jobs': ['finance', 'software']}]
+# we will refer to this as a "CV".
+cv1 = [{'user': 'john', 'jobs': ['analyst', 'engineer']},
+      {'user': 'jane', 'jobs': ['finance', 'software']}]
+
+cv2 = [{'user': 'jess', 'jobs': ['analyst', 'finance']},
+      {'user': 'jeremy', 'jobs': ['data science', 'software']}]
+list_of_cv = [cv1, cv2]
 #
 # 4)
 # Create a function called "has_experience_as"
@@ -8,7 +23,9 @@
 # The function should return a list of strings
 # representing the usernames of every user that
 # has worked as job_title.
-
+def has_experience_as(list_of_cv, job_title):
+    candidate = [i['user'] for i in list_of_cv if job_title in i['jobs']]
+    return candidate
 
 
 #
@@ -19,8 +36,15 @@
 # keys are the job titles and the values
 # are the number of users that have done
 # that job.
-
-
+def job_counts(list_of_cv):
+    jobcount = dict()
+    for cv in cvs:
+        for job in cv['jobs']:
+            if jobcount.get(job)!=None:
+                jobcount[job] += 1
+            else:
+                jobcount[job] = 1
+    return jobcount
 
 #
 # 6)
@@ -32,3 +56,8 @@
 #
 # HINT: You should probably use your "job_counts"
 # function!
+def most_popular_job(list_of_cv):
+    popular = ()
+    rank = job_counts(list_of_cv)
+    popular = sorted(rank, reverse = True)
+    return popular
